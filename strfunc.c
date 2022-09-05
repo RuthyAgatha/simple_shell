356e424adb27a13f48e7636f06586c651993119c
Isaac Katusabe <isaackatusabe@gmail.com>
2022-09-05 19:17:20 +0300

    Create strfunc.c

---
+++ strfunc.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+
+/**
+ * _puts - writes a string to standard output
+ * @str: string to write
+ *
+ * Return: number of chars printed or -1 on failure
+ */
+ssize_t _puts(char *str)
+{
+	ssize_t num, len;
+
+	num = _strlen(str);
+	len = write(STDOUT_FILENO, str, num);
+	if (len != num)
+	{
+		perror("Fatal Error");
+		return (-1);
+	}
+	return (len);
+}
+
+/**
+ * _strdup - returns pointer to new mem alloc space which contains copy
+ * @strtodup: string to be duplicated
+ * Return: a pointer to the new duplicated string
+ */
+char *_strdup(char *strtodup)
+{
+	char *copy;
+
+		int len, i;
+
+	if (strtodup == 0)
+		return (NULL);
+
+	for (len = 0; strtodup[len]; len++)
+		;
+	copy = malloc((len + 1) * sizeof(char));
+
+	for (i = 0; i <= len; i++)
+		copy[i] = strtodup[i];
+
+	return (copy);
+}
