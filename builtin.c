0976211156932311b2d8268aeda0cb7d1531182e
Isaac Katusabe <isaackatusabe@gmail.com>
2022-09-05 12:08:00 +0300

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -32,3 +32,32 @@ int (*get_builtin(char *command))(char **args, char **front)
 	}
 	return (funcs[i].f);
 }
+
+* @vars: variables
+ * Return: void
+ */
+void new_exit(vars_t *vars)
+{
+	int status;
+
+	if (_strcmpr(vars->av[0], "exit") == 0 && vars->av[1] != NULL)
+	{
+		status = _atoi(vars->av[1]);
+		if (status == -1)
+		{
+			vars->status = 2;
+			print_error(vars, ": Illegal number: ");
+			_puts2(vars->av[1]);
+			_puts2("\n");
+			free(vars->commands);
+			vars->commands = NULL;
+			return;
+		}
+		vars->status = status;
+	}
+	free(vars->buffer);
+	free(vars->av);
+	free(vars->commands);
+	free_env(vars->env);
+	exit(vars->status);
+}
